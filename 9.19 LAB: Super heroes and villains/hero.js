80b6f40d5356a5883090e2285cf5c80c732c62da
Matthew Henao <66018558+hanene152@users.noreply.github.com>
2024-07-21 15:49:38 -0400

    Add files via upload

---
+++ 9.19 LAB: Super heroes and villains/hero.js
@@ -0,0 +1,29 @@
+
+class SuperHuman {
+   constructor(name, powerLevel) {
+      this.name = name;
+      this.powerLevel = powerLevel;
+   }
+}
+
+class SuperHero extends SuperHuman {
+   constructor(name, alias, powerLevel) {
+      super(name, powerLevel);
+      this.alias = alias;
+   }
+
+   battle(villain) {
+      return this.powerLevel >= villain.powerLevel;
+   }
+}
+
+class SuperVillain extends SuperHuman {
+   constructor(name, alias, powerLevel) {
+      super(name, powerLevel);
+      this.alias = alias;
+   }
+
+   getEvilChuckle() {
+      return "Ha ha ha!";
+   }
+}
